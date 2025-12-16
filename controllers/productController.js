c388b8d8a613e96663d85988db5f1bff2a12d58b
tharu-008 <rptharurajapaksha@gmail.com>
2025-12-16 17:49:42 +0530

    product searching feature implementation started

--- controllers/productController.js
+++ controllers/productController.js
@@ -153,3 +153,7 @@ export async function getProductById(req, res) {
 	res.status(500).json({ message: "Error fetching product", error: error });
 	}
 }
+
+export async function searchProducts (req,res){
+	
+}
