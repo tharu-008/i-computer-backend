0ac629b263dbbd162a83f774f4fa35fb8f32bad5
tharu-008 <rptharurajapaksha@gmail.com>
2025-12-16 19:35:17 +0530

    Day 11 last part

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(authorizeUser)
 
 app.use("/users", userRouter)
 app.use("/products", productRouter)
-function start(){
-    console.log("Server started on port 3000")
-}
 
-app.listen(3000, start)
+app.listen(3000, () =>{
+    console.log("Server started on port 3000")
+})
